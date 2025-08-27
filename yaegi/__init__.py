e25500f44d3c685066dd8f21cc9221bd13908a9c
raeist <heyiamraeist@gmail.com>
2025-08-27 09:44:37 +0530

    Create __init__.py

---
+++ yaegi/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "0.1.0"
+__author__ = "Yaegi Team"
+__email__ = "support@yaegi.dev"
+__license__ = "MIT"
+__description__ = "A comprehensive Vedic Astrology (Jyotish) library for Python"
+
+from yaegi.models.chart import KundaliChart
+from yaegi.models.planet import Planet
+from yaegi.models.house import House
+from yaegi.models.dasha import DashaPeriod
+from yaegi.calculations.kundali import KundaliGenerator
+from yaegi.calculations.panchang import PanchangGenerator
+from yaegi.calculations.yogas import YogaDetector
+from yaegi.calculations.compatibility import CompatibilityAnalyzer
+
+__all__ = [
+    "KundaliChart",
+    "Planet",
+    "House", 
+    "DashaPeriod",
+    "KundaliGenerator",
+    "PanchangGenerator",
+    "YogaDetector",
+    "CompatibilityAnalyzer",
+]
